05a4b9dc482d1e87137713b7fcd32d37afe78760
suyeon <rlatndus9911@naver.com>
2022-02-17 03:59:29 +0900

    [FEAT] navigation controller 세팅

--- InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/MainTabController.swift
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/MainTabController.swift
@@ -21,22 +21,32 @@ class MainTabController: UITabBarController {
 
     func configureViewController() {
 
-        let feed = FeedController()
+        let feed = templateNavigationController(unselectedImage: UIImage(named: "home_unselected"), selectedImage: UIImage(named: "home_selected"), rootViewController: FeedController())
 
-        let search = SearchController()
+        let search = templateNavigationController(unselectedImage: UIImage(named: "search_unselected"), selectedImage: UIImage(named: "search_selected"), rootViewController: SearchController())
 
-        let imageSelector = ImageSelectorController()
+        let imageSelector = templateNavigationController(unselectedImage: UIImage(named: "plus_unselected"), selectedImage: UIImage(named: "plus_unselected"), rootViewController: ImageSelectorController())
 
-        let notification = NotificationController()
+        let notifications = templateNavigationController(unselectedImage: UIImage(named: "like_unselected"), selectedImage: UIImage(named: "like_selected"), rootViewController: NotificationController())
 
-        let profile = ProfileController()
+        let profile = templateNavigationController(unselectedImage: UIImage(named: "profile_unselected"), selectedImage: UIImage(named: "profile_selected"), rootViewController: ProfileController())
 
         // UITabBarController inherit : An array of the root view controllers displayed by the tab bar interface
         // represents an instance of the controller
-        viewControllers = [feed, search, imageSelector, notification, profile]
+        viewControllers = [feed, search, imageSelector, notifications, profile]
 
         tabBar.backgroundColor = .white
+        tabBar.tintColor = .black
+    }
 
+    // 모든 탭에 대하여 각각 rootViewController로 설정하는 navigationcontroller을 생성하는 함수 !
+    func templateNavigationController(unselectedImage: UIImage?, selectedImage: UIImage?, rootViewController: UIViewController) -> UINavigationController {
+        let nav = UINavigationController(rootViewController: rootViewController)
+        nav.tabBarItem.image = unselectedImage
+        nav.tabBarItem.selectedImage = selectedImage
+        nav.navigationBar.tintColor = .black
+        nav.navigationBar.backgroundColor = .white
+        return nav
     }
 
 }
