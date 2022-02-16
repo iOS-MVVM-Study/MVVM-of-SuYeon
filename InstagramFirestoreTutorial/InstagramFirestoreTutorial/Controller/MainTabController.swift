7d6710b2527f81d16410233bb1c88bbb5ad6e765
suyeon <rlatndus9911@naver.com>
2022-02-17 02:25:41 +0900

    [FEAT] rootViewController 설정

---
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/MainTabController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by 김수연 on 2022/02/17.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+}
