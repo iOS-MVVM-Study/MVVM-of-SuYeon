5cb1319134dac4efe3fc6eb63f47145e389b8a5e
suyeon <rlatndus9911@naver.com>
2022-02-25 02:25:04 +0900

    [ADD] 로그인 , 회원가입 controller 추가

---
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/Authentication/LoginController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by 김수연 on 2022/02/24.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    // MARK: - Properties
+
+    // MARK: - LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+
+    // MARK: - Helpers
+
+    func configureUI() {
+        view.backgroundColor = .white
+    }
+}
---
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,22 @@
+//
+//  RegistrationController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by 김수연 on 2022/02/24.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+
+    // MARK: - Properties
+
+    // MARK: - LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+
+}
